12680c10df9f227c41bd478ae9b827eb170791bb
Rubén Blanco <r.blanco.3d@gmail.com>
2024-10-22 22:44:16 +0200

    Estructura principal creada con Spring Initializr

---
+++ src/test/java/org/factoriaf5/java_list/JavaListApplicationTests.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.java_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
